1b42d76f492d5ba4299f7892c9bc97c4bd0088bf
GEA2SF <atanas.gegov@bosch.io>
2023-02-08 17:37:02 +0200

    Added a proper shutdown of the benchmark service.

--- src/main/java/com/akg/springmvcwebfluxasync/service/Executor.java
+++ src/main/java/com/akg/springmvcwebfluxasync/service/Executor.java
@@ -1,9 +1,17 @@
 package com.akg.springmvcwebfluxasync.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StopWatch;
@@ -20,35 +28,60 @@ public class Executor {
 	private Config config;
 	
 	@Autowired
+	private ApplicationContext context;
+
+	@Autowired
 	private AsyncService asyncService;
+	
+	private ScheduledExecutorService shutdowner = Executors.newScheduledThreadPool(1);
 
 	@EventListener(ApplicationReadyEvent.class)
-	public void exec() throws InterruptedException {
+	public void exec() {
 		log.info("Started.");
 		StopWatch stopWatch = new StopWatch();
 		stopWatch.start();
 
-		if( config.getType().equals(Config.ExecutionType.ASYNC.getValue()) ) {
+		if (config.getType().equals(Config.ExecutionType.ASYNC.getValue())) {
+
 			async();
-		} else if( config.getType().equals(Config.ExecutionType.MVC.getValue()) ) {
-		} else if(config.getType().equals(Config.ExecutionType.WEB_FLUX.getValue())) {
+		} else if (config.getType().equals(Config.ExecutionType.MVC.getValue())) {
 			
+			// MVC code is here
+		} else if (config.getType().equals(Config.ExecutionType.WEB_FLUX.getValue())) {
+			
+			// WEB_FLUX code is here
 		} else {
 			log.warn("Please, setup properly the configuration file - application.yml.");
 		}
+		
 		stopWatch.stop();
-		log.info("{}, time execution: {}", config.getType(), stopWatch.getTotalTimeMillis());
+		log.info("{}, time execution: {} ms.", config.getType(), stopWatch.getTotalTimeMillis());
 		log.info("Done.");
+
+		int shutdownAfter = 1;
+		log.info("The application will shutdown in {} seconds.", shutdownAfter);
+		this.shutdown(shutdownAfter);
+	}
+
+	private void shutdown(int seconds) {
+		Runnable shutdownerTask = () -> { 
+			((ConfigurableApplicationContext) context).close();
+			System.exit(0);
+		};
+		shutdowner.schedule(shutdownerTask, seconds, TimeUnit.SECONDS);
 	}
 	
-	private void async() {
+	private List<CompletableFuture<String>> async() {
+		List<CompletableFuture<String>> list = new ArrayList<>(); 
 		IntStream.range(0, config.getNumberOfExecutions()).forEachOrdered(i -> {
 			try {
-				asyncService.doWork(i);
+				list.add( asyncService.doWork(i) );
 			} catch (InterruptedException e) {
 				log.error("Something wrong happened.", e);
 				Thread.currentThread().interrupt();
 			}
 		});
+		
+		return list;
 	}
 }
